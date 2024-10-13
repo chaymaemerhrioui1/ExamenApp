4416a7dd489ea1158a461d1b0f997dc81bb61291
Chaymae Merhrioui <128318349+chaymaemerhrioui1@users.noreply.github.com>
2024-10-13 15:57:02 +0200

    Delete java/com/ensah directory

--- java/com/ensah/core/dao/SalleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ensah.core.dao;
-
-import com.ensah.core.bo.Salle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalleRepository extends JpaRepository<Salle, Long> {
-}
-
--- java/com/ensah/core/dao/SemestreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ensah.core.dao;
-
-import com.ensah.core.bo.Semestre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SemestreRepository extends JpaRepository<Semestre, Long> {
-}
